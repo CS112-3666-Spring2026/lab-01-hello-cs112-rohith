2d3f26ed72d50952b3cb236e2686aeb14773087f
rmekala3623 <rmekala3623@student.miracosta.edu>
2026-02-02 01:55:12 +0000

    Changed initial print statement. Created "PrintHello" and "ReturnSum" Methods. Created "Book" class.

--- Main.java
+++ Main.java
@@ -1,5 +1,66 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Welcome to CS 112!");
+    }
+
+    static void PrintHello() {
+        System.out.println("Hello!");
+    }
+
+    static double ReturnSum(double x, double y) {
+        return x + y;
+    }
+}
+
+class Book {
+    String title;
+
+    Book()
+    {
+        title = "";
+    }
+
+    Book(String userTitle)
+    {
+        title = userTitle;
+    }
+    
+    public String toString()
+    {
+        return ("Book Title: " + title);
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public void setTitle(String newTitle)
+    {
+        title = newTitle;
+    }
+
+    public boolean equals(Object o)
+    {
+        if(o == this)
+        {
+            return true;
+        }
+        if(o == null)
+        {
+            return false;
+        }
+        if (!(o instanceof Book))
+        {
+            return false;
+        }
+
+        Book other = (Book) o;
+        if(this.title != other.title)
+        {
+            return false;
+        }
+
+        return true;
     }
 }
